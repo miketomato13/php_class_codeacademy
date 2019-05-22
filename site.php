b665c78c53a75b24f6fe900162228c1bc6d6f2ed
MikeTomato <mike@costabellarealtygroup.com>
2019-05-22 16:09:27 -0400

    mike dane's php course

---
+++ site.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+    // WORKING WITH STRINGS
+    echo "Giraffe Academy";
+
+    // DATA TYPES
+    /* $phrase = "To be or not to be"; //string
+    $integer = 35; //integer, positive or negative
+    $float = 3.5; //any numnber with a decimal point
+    $boolean = true; //true or false value
+    null; //no value, is it's own data type */
+
+
+
+    // VARIABLES
+    /*  $characterName = "John";
+      $characterAge  = 38;
+      echo "There was once a  man named $characterName <br>";
+      echo "He was $characterAge years old <br>";
+      echo "He really liked the name $characterName <br>";
+      echo "But he didn't like being $characterAge <br>"; */
+    ?>
+  </body>
+</html>
